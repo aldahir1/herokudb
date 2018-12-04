d7a2352fe4b9ce4b63abe7829af9d4716775f361
aldahir1 <33230105+aldahir1@users.noreply.github.com>
2018-12-04 10:04:09 -0600

    Update creartabla.php

--- creartabla.php
+++ creartabla.php
@@ -12,15 +12,17 @@ function getxd($insertar) {
   $variable = json_decode($_GET[$insertar]);
   return $variable;
 }
+
+$asdfg = getxd("insertar");
 // preparar consultas
 pg_prepare($conexion, "sql1", 'DROP TABLE IF EXISTS datosunity');
 pg_prepare($conexion, "sql2", 'CREATE TABLE datosunity (nombre VARCHAR(30), numero INT)');
 pg_prepare($conexion, "sql3", 'INSERT INTO gente (nombre, numero) VALUES ($1, $2)');
 pg_prepare($conexion, "sql4", 'SELECT * FROM datosunity');
 // ejecutar consultas
-//pg_execute($conexion, "sql1", array());
-//pg_execute($conexion, "sql2", array());
-//pg_execute($conexion, "sql3", array(" . $ variable", 23));
+pg_execute($conexion, "sql1", array());
+pg_execute($conexion, "sql2", array());
+pg_execute($conexion, "sql3", array(" . $asdfg . ", 23));
 $resultado = pg_execute($conexion, "sql4", array());
 // indicar que el resultado es JSON
 header("Content-type: application/json; charset=utf-8");
