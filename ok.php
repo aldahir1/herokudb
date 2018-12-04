7d759694de5b017c0c39fb5d608f0c8b1d58db59
aldahir1 <33230105+aldahir1@users.noreply.github.com>
2018-12-04 10:10:05 -0600

    Update ok.php

--- ok.php
+++ ok.php
@@ -7,15 +7,8 @@ $conexion = pg_connect(
   " dbname=" . substr($datos["path"], 1) . 
   " user=" . $datos["user"] . 
   " password=" . $datos["pass"]);
-function getxd($insertar) {
-  $variable = json_decode($_GET[$insertar]);
-  return $variable;
-}
-$asdfg = getxd("insertar");
+
 // preparar consultas
-pg_prepare($conexion, "sql1", 'DROP TABLE IF EXISTS datosunity');
-pg_prepare($conexion, "sql2", 'CREATE TABLE datosunity (nombre VARCHAR(30), numero INT)');
-pg_prepare($conexion, "sql3", 'INSERT INTO datosunity (nombre, numero) VALUES ($1, $2)');
 pg_prepare($conexion, "sql4", 'SELECT * FROM datosunity');
 // ejecutar consultas
 pg_execute($conexion, "sql1", array());
