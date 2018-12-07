1f88e57f9337afc7e7aec9d969dfe15ba567178a
aldahir1 <33230105+aldahir1@users.noreply.github.com>
2018-12-07 09:41:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 	<html>
 		<head>
-			<title>Pagina Prueba</title>
+			<title>Pagina de World of Titans</title>
 		</head>
 		<body>
 			<h1>Hola</h1>
-			<p>Hola Mundo, este es el primer cambio en el archivo</p>
-			<center><img src="img/hola.png" alt="" width="460" height="410"></center>
-			<h2>Hola de nuevo</h2>
-			<center><img src="img/desert.jpeg" alt="" width="460" height="410"></center>
-			<h2>Un diagrama extraño</h2>
-			<center><img src="img/captura.png" alt="" width="460" height="410"></center>
-			<h3>Hipertexto</h3>
-				<p align="justify">El hipertexto es una tecnologia que organiza una base de informacion en bloques distintos de contenidos, conectados a traves
-		de una serie de enlaces cuya activacion o seleccion provoca la recuperacion de informacion [Diaz et al, 1996].</p>
 		</body>
 	</html>
