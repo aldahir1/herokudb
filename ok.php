a26f67aa27f2dba107a8ea367f7a3cf035a851f6
aldahir1 <33230105+aldahir1@users.noreply.github.com>
2018-12-07 09:50:56 -0600

    Update ok.php

--- ok.php
+++ ok.php
@@ -9,8 +9,8 @@ $conexion = pg_connect(
   " password=" . $datos["pass"]);
 
 // preparar consultas
-pg_prepare($conexion, "sql4", 'SELECT * FROM XerathDatos');
-pg_prepare($conexion, "sql3", 'SELECT * FROM XerathDatosxd');
+pg_prepare($conexion, "sql4", 'SELECT * FROM WOTDatos');
+pg_prepare($conexion, "sql3", 'SELECT * FROM WOTDatos1');
 // ejecutar consultas
 $resultado = pg_execute($conexion, "sql3", array());
 $resultado1 = pg_execute($conexion, "sql4", array());
@@ -21,14 +21,12 @@ header('Access-Control-Allow-Origin: *');
 // imprimir resultado
 $gente = array();
 while ($fila = pg_fetch_assoc($resultado)) {
-  $fila["muertes"] = intval($fila["muertes"]);
   array_push($gente, $fila);
 }
 echo json_encode($gente);
 
 $usuarios = array();
 while ($fila1 = pg_fetch_assoc($resultado1)) {
-  $fila1["usuario"] = intval($fila1["usuario"]);
   array_push($usuarios, $fila1);
 }
 echo json_encode($usuarios);
